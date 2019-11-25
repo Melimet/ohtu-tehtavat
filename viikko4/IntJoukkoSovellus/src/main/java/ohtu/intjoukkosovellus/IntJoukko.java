40f72802cb832a51faba6f53047e01a1995339b0
Erkka Rahikainen <Erkka.Rahikainen@gmail.com>
2019-11-25 17:54:23 +0200

    Loput tehty

---
+++ viikko4/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
@@ -0,0 +1,185 @@
+
+package ohtu.intjoukkosovellus;
+
+public class IntJoukko {
+
+    private final static int KAPASITEETTI = 5, // aloitustalukon koko
+                            OLETUSKASVATUS = 5;  // luotava uusi taulukko on 
+    // näin paljon isompi kuin vanha
+    private int kasvatuskoko;     // Uusi taulukko on tämän verran vanhaa suurempi.
+    private int[] ljono;      // Joukon luvut säilytetään taulukon alkupäässä. 
+    private int alkioidenLkm;    // Tyhjässä joukossa alkioiden_määrä on nolla. 
+
+    public IntJoukko() {
+        ljono = new int[KAPASITEETTI];
+        for (int i = 0; i < ljono.length; i++) {
+            ljono[i] = 0;
+        }
+        alkioidenLkm = 0;
+        this.kasvatuskoko = OLETUSKASVATUS;
+    }
+
+    public IntJoukko(int kapasiteetti) {
+        if (kapasiteetti < 0) {
+            return;
+        }
+        ljono = new int[kapasiteetti];
+        for (int i = 0; i < ljono.length; i++) {
+            ljono[i] = 0;
+        }
+        alkioidenLkm = 0;
+        this.kasvatuskoko = OLETUSKASVATUS;
+
+    }
+    
+    
+    public IntJoukko(int kapasiteetti, int kasvatuskoko) {
+        if (kapasiteetti < 0) {
+            throw new IndexOutOfBoundsException("Kapasitteetti väärin");//heitin vaan jotain :D
+        }
+        if (kasvatuskoko < 0) {
+            throw new IndexOutOfBoundsException("kapasiteetti2");//heitin vaan jotain :D
+        }
+        ljono = new int[kapasiteetti];
+        for (int i = 0; i < ljono.length; i++) {
+            ljono[i] = 0;
+        }
+        alkioidenLkm = 0;
+        this.kasvatuskoko = kasvatuskoko;
+
+    }
+
+    public void lisaa(int luku) {
+
+        int eiOle = 0;
+        if (alkioidenLkm == 0) {
+            ljono[0] = luku;
+            alkioidenLkm++;
+            return;
+        }if (!kuuluu(luku)) {
+            ljono[alkioidenLkm] = luku;
+            alkioidenLkm++;
+            if (alkioidenLkm % ljono.length == 0) {
+                int[] taulukkoOld = new int[ljono.length];
+                taulukkoOld = ljono;
+                kopioiTaulukko(ljono, taulukkoOld);
+                ljono = new int[alkioidenLkm + kasvatuskoko];
+                kopioiTaulukko(taulukkoOld, ljono);
+            }
+        }
+    }
+
+    public boolean kuuluu(int luku) {
+        int on = 0;
+        for (int i = 0; i < alkioidenLkm; i++) {
+            if (luku == ljono[i]) {
+                on++;
+            }
+        }
+        return on > 0;
+    }
+
+    public void poista(int luku) {
+        int kohta = -1;
+        int apu;
+        for (int i = 0; i < alkioidenLkm; i++) {
+            if (luku == ljono[i]) {
+                kohta = i; //siis luku löytyy tuosta kohdasta :D
+                ljono[kohta] = 0;
+                break;
+            }
+        }
+        if (kohta != -1) {
+            for (int j = kohta; j < alkioidenLkm - 1; j++) {
+                apu = ljono[j];
+                ljono[j] = ljono[j + 1];
+                ljono[j + 1] = apu;
+            }
+            alkioidenLkm--;
+        }
+
+
+    }
+
+    private void kopioiTaulukko(int[] vanha, int[] uusi) {
+        for (int i = 0; i < vanha.length; i++) {
+            uusi[i] = vanha[i];
+        }
+
+    }
+
+    public int mahtavuus() {
+        return alkioidenLkm;
+    }
+
+
+    @Override
+    public String toString() {
+        if (alkioidenLkm == 0) {
+            return "{}";
+        } else if (alkioidenLkm == 1) {
+            return "{" + ljono[0] + "}";
+        } else {
+            StringBuilder tuotos = new StringBuilder("{");
+            for (int i = 0; i < alkioidenLkm - 1; i++) {
+                tuotos.append(ljono[i]);
+                tuotos.append(", ");
+            }
+            tuotos.append(ljono[alkioidenLkm - 1]);
+            tuotos.append("}");
+            return tuotos.toString();
+        }
+    }
+
+    public int[] toIntArray() {
+        int[] taulu = new int[alkioidenLkm];
+        for (int i = 0; i < taulu.length; i++) {
+            taulu[i] = ljono[i];
+        }
+        return taulu;
+    }
+   
+
+    public static IntJoukko yhdiste(IntJoukko a, IntJoukko b) {
+        IntJoukko x = new IntJoukko();
+        int[] aTaulu = a.toIntArray();
+        int[] bTaulu = b.toIntArray();
+        for (int value : aTaulu) {
+            x.lisaa(value);
+        }
+        for (int value : bTaulu) {
+            x.lisaa(value);
+        }
+        return x;
+    }
+
+    public static IntJoukko leikkaus(IntJoukko a, IntJoukko b) {
+        IntJoukko y = new IntJoukko();
+        int[] aTaulu = a.toIntArray();
+        int[] bTaulu = b.toIntArray();
+        for (int item : aTaulu) {
+            for (int value : bTaulu) {
+                if (item == value) {
+                    y.lisaa(value);
+                }
+            }
+        }
+        return y;
+
+    }
+    
+    public static IntJoukko erotus ( IntJoukko a, IntJoukko b) {
+        IntJoukko z = new IntJoukko();
+        int[] aTaulu = a.toIntArray();
+        int[] bTaulu = b.toIntArray();
+        for (int value : aTaulu) {
+            z.lisaa(value);
+        }
+        for (int value : bTaulu) {
+            z.poista(value);
+        }
+ 
+        return z;
+    }
+        
+}
--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -1,10 +1,47 @@
 package ohtu.verkkokauppa;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 public class KauppaTest {
 
 
+    Pankki pankki;
 
+    Varasto varasto;
+    Viitegeneraattori viitegeneraattori;
+    Kauppa kauppa;
+
+    @Before
+    public void setUp(){
+        pankki = mock(Pankki.class);
+        varasto = mock(Varasto.class);
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(42);
+        kauppa = new Kauppa(varasto, pankki, viite);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        kauppa.aloitaAsiointi();
+}
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan(){
+
+        kauppa.lisaaKoriin(1);
+        kauppa.poistaKorista(1);
+
+        verify(varasto, times(1)).palautaVarastoon(any());
+
+    }
 }
